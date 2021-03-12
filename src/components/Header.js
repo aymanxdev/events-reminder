258914878e4245ac04a758b79003a02b2bc96dc2
aymanxdev <ayman.bacc@gmail.com>
2021-03-12 23:22:36 +0000

    add header and clean up

---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "../assets/styles/header.css";
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="left-side">
+        <h2 className="logo">Events Reminder</h2>
+      </div>
+      <div className="right-side">
+        <label class="switch">
+          <input type="checkbox" />
+          <span class="slider round"></span>
+        </label>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
